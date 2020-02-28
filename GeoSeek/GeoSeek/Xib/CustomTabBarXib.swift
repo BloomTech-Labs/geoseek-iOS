268ab3aef65be5e5528dc84c36d9a3018f188794
Jerry Haaser <jerry.haaser@hotmail.com>
2020-02-28 15:57:58 -0600

    Created Xib
    Created CustomTabBar Xib
    
    Created CustomTabBarXib swift file and connected outlets

---
+++ GeoSeek/GeoSeek/Xib/CustomTabBarXib.swift
@@ -0,0 +1,38 @@
+//
+//  CustomTabBarXib.swift
+//  GeoSeek
+//
+//  Created by Jerry haaser on 2/28/20.
+//  Copyright © 2020 Brandi Bailey. All rights reserved.
+//
+
+import UIKit
+
+class CustomTabBarXib: UIView {
+
+    @IBOutlet var contentView: UIView!
+    @IBOutlet weak var addGemMapViewButton: UIButton!
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        commonInit()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        commonInit()
+    }
+    
+    private func commonInit() {
+        let nib = UINib(nibName: "CustomTabBarXib", bundle: nil)
+        nib.instantiate(withOwner: self, options: nil)
+        addSubview(contentView)
+        contentView.frame = self.bounds
+        contentView.autoresizingMask = [.flexibleHeight, .flexibleWidth]
+    }
+    
+    @IBAction func addGemMapViewButtonTapped(_ sender: Any) {
+        print("add gem map")
+    }
+    
+}
