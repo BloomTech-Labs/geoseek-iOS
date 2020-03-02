cc7e73e7149584627e03fcc4db864e4ba01944ca
Dan Morse <djmthedev@gmail.com>
2020-03-02 10:46:10 -0800

    added show Gem functionality

--- GeoSeek/GeoSeek/View Controllers/ViewController.swift
+++ GeoSeek/GeoSeek/View Controllers/ViewController.swift
@@ -11,12 +11,13 @@ import Mapbox
 
 class ViewController: UIViewController, Storyboarded {
     
-//    @IBOutlet weak var myMapView: MGLMapView!
+    //    @IBOutlet weak var myMapView: MGLMapView!
     @IBOutlet weak var customTabBarXib: CustomTabBarXib!
-    @IBOutlet weak var mapXib: MapXib!
+    @IBOutlet weak var mapView: MGLMapView!
     
     
     weak var coordinator: MainCoordinator?
+    var gems: [Gem] = []
     
     //@IBOutlet weak var displayMapView: MapXib!
     
@@ -24,7 +25,7 @@ class ViewController: UIViewController, Storyboarded {
     
     var pressedLocation:CLLocation? = nil {
         didSet{
-//                continueButton.isEnabled = true
+            //                continueButton.isEnabled = true
             print("pressedLocation was set")
         }
     }
@@ -41,28 +42,59 @@ class ViewController: UIViewController, Storyboarded {
         coordinator?.navControllers[0].setToolbarHidden(true, animated: false)
         customTabBarXib.coordinator = coordinator
         //mapXib.coordinator = coordinator
-        
+        fetchGems()
+        //        NetworkController.shared.fetchGems { result in
+        //            switch result {
+        //            case .failure(let error):
+        //                print("Oops!:", error)
+        //            case .success(let gems):
+        //                gems.compactMap { (print("ViewController:", $0.title)) }
+        //            }
+    }
+    
+    func fetchGems() {
         NetworkController.shared.fetchGems { result in
             switch result {
             case .failure(let error):
-                print("Oops!:", error)
+                print("ViewController.fetchGems Error: \(error)")
             case .success(let gems):
-                gems.compactMap { (print("ViewController:", $0.title)) }
+                self.gems = gems
+                DispatchQueue.main.async {
+                    self.configureMapView()
+                }
             }
         }
-        
-//        let mapView = MGLMapView(frame: view.bounds, styleURL: MGLStyle.satelliteStyleURL)
-//        mapView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
-//        mapView.setCenter(CLLocationCoordinate2D(latitude: 33.812794, longitude: -117.9190981), zoomLevel: 15, animated: false)
-        
-//        let lpgr = UILongPressGestureRecognizer(target: self, action: #selector(handleLongPress))
-//        lpgr.minimumPressDuration = 0.5
-//        lpgr.delaysTouchesBegan = false
-//        mapView.addGestureRecognizer(lpgr)
-        
-       // view.addSubview(mapView)
     }
     
+    func configureMapView() {
+        mapView.styleURL = MGLStyle.outdoorsStyleURL
+        mapView.tintColor = .darkGray
+        mapView.centerCoordinate = CLLocationCoordinate2D(latitude: 18, longitude: -64)
+        mapView.zoomLevel = 3
+        mapView.delegate = self
+        
+        var pointAnnotations: [MGLPointAnnotation] = []
+        for gem in gems {
+            let point = MGLPointAnnotation()
+            point.coordinate = CLLocationCoordinate2D(latitude: gem.latitude, longitude: gem.longitude)
+            point.title = "\(gem.title ?? "No Title")"
+            pointAnnotations.append(point)
+        }
+        mapView.addAnnotations(pointAnnotations)
+    }
+    
+    //        let mapView = MGLMapView(frame: view.bounds, styleURL: MGLStyle.satelliteStyleURL)
+    //        mapView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+    //        mapView.setCenter(CLLocationCoordinate2D(latitude: 33.812794, longitude: -117.9190981), zoomLevel: 15, animated: false)
+    
+    //        let lpgr = UILongPressGestureRecognizer(target: self, action: #selector(handleLongPress))
+    //        lpgr.minimumPressDuration = 0.5
+    //        lpgr.delaysTouchesBegan = false
+    //        mapView.addGestureRecognizer(lpgr)
+    
+    // view.addSubview(mapView)
+}
+
 //    @objc func handleLongPress(gestureReconizer: UILongPressGestureRecognizer) {
 //        if gestureReconizer.state != UIGestureRecognizer.State.ended {
 //        } else {
@@ -80,7 +112,29 @@ class ViewController: UIViewController, Storyboarded {
 //
 //        }
 //    }
-    
-    
-}
 
+
+
+
+extension ViewController: MGLMapViewDelegate {
+    func mapView(_ mapView: MGLMapView, viewFor annotation: MGLAnnotation) -> MGLAnnotationView? {
+        guard annotation is MGLPointAnnotation else { return nil }
+        
+        let reuseIdentifier = "\(annotation.coordinate.latitude)\(annotation.coordinate.longitude)"
+        
+        var annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: reuseIdentifier)
+        let hue = CGFloat((abs(Double(annotation.coordinate.longitude).rounded())) / 90)
+        let annotationColor = UIColor(hue: hue, saturation: 0.5, brightness: 1, alpha: 1)
+        
+        if annotationView == nil {
+            annotationView = CustomAnnotationView(reuseIdentifier: reuseIdentifier)
+            annotationView?.backgroundColor = annotationColor
+            annotationView?.bounds = CGRect(x: 0, y: 0, width: 20, height: 20)
+        }
+        return annotationView
+    }
+    
+    func mapView(_ mapView: MGLMapView, annotationCanShowCallout annotation: MGLAnnotation) -> Bool {
+        return true
+    }
+}
