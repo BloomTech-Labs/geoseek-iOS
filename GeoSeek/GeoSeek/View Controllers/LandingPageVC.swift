1036a4dd868ea0e676f8a4e748a2b03f2b058f8b
Brandi <blubrandi0@gmail.com>
2020-03-04 15:59:19 -0700

    LandingPageVC & CoreLocation
    Created LandingPageVC and added CoreLocation permission request to get users current location.  Added LandingPageVC to MainCoordinator.  TODO:  Switch on Location Auth Access in MainCoordinator to decide if this scene is necessary upon running app.

---
+++ GeoSeek/GeoSeek/View Controllers/LandingPageVC.swift
@@ -0,0 +1,42 @@
+//
+//  LandingPageVC.swift
+//  GeoSeek
+//
+//  Created by Brandi Bailey on 3/4/20.
+//  Copyright © 2020 Brandi Bailey. All rights reserved.
+//
+
+import UIKit
+import CoreLocation
+
+class LandingPageVC: UIViewController, CLLocationManagerDelegate, Storyboarded {
+    
+    var locationManager: CLLocationManager?
+    weak var coordinator: MainCoordinator?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        locationManager = CLLocationManager()
+        locationManager?.delegate = self
+        
+    }
+    
+
+    @IBAction func setLocationTapped(_ sender: Any) {
+        
+        locationManager?.requestAlwaysAuthorization()
+        
+    }
+
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        if status == .authorizedAlways {
+            if CLLocationManager.isMonitoringAvailable(for: CLBeaconRegion.self) {
+                if CLLocationManager.isRangingAvailable() {
+                    print("location:", locationManager?.monitoredRegions)
+                }
+            }
+        }
+    }
+
+}
