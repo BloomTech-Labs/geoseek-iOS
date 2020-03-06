d9566479f6d109a0d8588a5d95a3fbeb65f2fa09
Dan Morse <djmthedev@gmail.com>
2020-03-06 11:52:25 -0800

    GemsMapCoordinator getting rid of unused code

--- GeoSeek/GeoSeek/Coordinator/CreateGemCoordinator.swift
+++ GeoSeek/GeoSeek/Coordinator/CreateGemCoordinator.swift
@@ -10,7 +10,6 @@ import UIKit
 import CoreLocation
 
 protocol CreateGemCoordinatorDelegate {
-    func presentChooseLocationVC()
     func presentGemsMap()
 }
 
--- GeoSeek/GeoSeek/Coordinator/GemsMapCoordinator.swift
+++ GeoSeek/GeoSeek/Coordinator/GemsMapCoordinator.swift
@@ -19,13 +19,7 @@ class GemsMapCoordinator: BaseCoordinator {
     
     let window: UIWindow
     var navigationController: UINavigationController?
-//    var userLocationLat: CLLocationDegrees?
-//    var userLocationLong: CLLocationDegrees?
-//    var setLocation: CLLocation?
     var userLocation: CLLocationCoordinate2D?
-    
-
-
     var delegate: GemsMapCoordinatorDelegate?
     
     init(window: UIWindow) {
@@ -35,23 +29,20 @@ class GemsMapCoordinator: BaseCoordinator {
     
     override func start() {
         let viewController = GemsMapVC.instantiate()
-//        navigationController = UINavigationController()
         viewController.coordinator = self
         viewController.delegate = delegate
-//        window.rootViewController = navigationController
         viewController.userLocation = userLocation
         navigationController?.isNavigationBarHidden = true
         navigationController?.setViewControllers([viewController], animated: true)
-//        navigationController?.pushViewController(viewController, animated: true)
         
-        if let vc = navigationController?.viewControllers.first {
-            print(vc.description)
-        } else {
-            let vc = GemsMapVC.instantiate()
-            vc.coordinator = self
-            vc.delegate = delegate
-            navigationController?.pushViewController(vc, animated: true)
-        }
+//        if let vc = navigationController?.viewControllers.first {
+//            print(vc.description)
+//        } else {
+//            let vc = GemsMapVC.instantiate()
+//            vc.coordinator = self
+//            vc.delegate = delegate
+//            navigationController?.pushViewController(vc, animated: true)
+//        }
     }
     
     func navigateToCreateGemCoordinator() {
