20b33a65421709e0eb8721f138a154b75bf256e8
Dan Morse <djmthedev@gmail.com>
2020-03-13 10:16:28 -0700

    Added RegisterCoordinator.swift

---
+++ GeoSeek/GeoSeek/Coordinator/RegisterCoordinator.swift
@@ -0,0 +1,14 @@
+//
+//  RegisterCoordinator.swift
+//  GeoSeek
+//
+//  Created by morse on 3/13/20.
+//  Copyright © 2020 Brandi Bailey. All rights reserved.
+//
+
+import Foundation
+
+class RegisterCoordinator: BaseCoordinator {
+    
+    
+}
