a71d3fda331606ca80e9eb353f45c5225c6e2d62
blubrandi <blubrandi0@gmail.com>
2020-03-12 20:05:35 -0600

    User Menu
    Created Nav Buttons Xib and added button to GemsMapVC

---
+++ GeoSeek/GeoSeek/Xib/NavButtonsXib.swift
@@ -0,0 +1,48 @@
+//
+//  NavButtonsXib.swift
+//  GeoSeek
+//
+//  Created by Brandi Bailey on 3/12/20.
+//  Copyright © 2020 Brandi Bailey. All rights reserved.
+//
+
+import UIKit
+
+class NavButtonsXib: UIView, Storyboarded {
+    
+    @IBOutlet weak var navButtonsView: UIView!
+    @IBOutlet weak var userMenuButton: UIButton!
+    
+    var delegate: GemsMapCoordinatorDelegate?
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        commonInit()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        commonInit()
+    }
+    
+    private func commonInit() {
+        
+        let nib = UINib(nibName: "NavButtonsXib", bundle: nil)
+        nib.instantiate(withOwner: self, options: nil)
+        addSubview(navButtonsView)
+        
+        navButtonsView.frame = self.bounds
+        navButtonsView.autoresizingMask = [.flexibleHeight, .flexibleWidth]
+        navButtonsView.backgroundColor = .clear
+        
+        let buttonColor = #colorLiteral(red: 0.9568627451, green: 0.2509803922, blue: 0.462745098, alpha: 1)
+        
+        navButtonsView.backgroundColor = buttonColor
+        navButtonsView.layer.cornerRadius = 20
+    }
+    
+    @IBAction func userMenuButtonTapped(_ sender: Any) {
+        print("Menu Button Tapped...")
+    }
+    
+}
