cea97b2a4a937ae6d7d9241a148c867800acdbd3
Dan Morse <djmthedev@gmail.com>
2020-03-13 11:24:18 -0700

    Added LogInCoordinator

---
+++ GeoSeek/GeoSeek/Coordinator/LogInCoordinator.swift
@@ -0,0 +1,14 @@
+//
+//  LogInCoordinator.swift
+//  GeoSeek
+//
+//  Created by morse on 3/13/20.
+//  Copyright © 2020 Brandi Bailey. All rights reserved.
+//
+
+import UIKit
+
+class LogInCoordinator: BaseCoordinator {
+    
+    
+}
