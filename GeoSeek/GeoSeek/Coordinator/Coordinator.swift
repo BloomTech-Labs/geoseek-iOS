daa1aa5bb60ca23bdb04ed42bf700fd8de76cee4
Dan Morse <djmthedev@gmail.com>
2020-03-14 10:07:27 -0700

    Coordinator got rid of unused properties

--- GeoSeek/GeoSeek/Coordinator/Coordinator.swift
+++ GeoSeek/GeoSeek/Coordinator/Coordinator.swift
@@ -11,8 +11,7 @@ import UIKit
 
 protocol Coordinator: class {
     
-    var childCoordinators: [Coordinator] { get set }
-    var navControllers: [UINavigationController] { get set }
+    var navigationController: UINavigationController { get set }
     
     func start()
 }
