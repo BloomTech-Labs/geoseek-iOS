f19747eab899183ba833bbcc2b0baf8a77129fa4
Dan Morse <djmthedev@gmail.com>
2020-03-18 15:26:54 -0700

    PasswordView added inits

--- GeoSeek/GeoSeek/Xib/PasswordView.swift
+++ GeoSeek/GeoSeek/Xib/PasswordView.swift
@@ -14,5 +14,21 @@ class PasswordView: UIView {
     @IBOutlet weak var hideButton: UIButton!
     @IBOutlet weak var textField: UITextField!
     
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
     
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        commonInit()
+    }
+    
+    func commonInit() {
+        let nib = UINib(nibName: "PasswordView", bundle: nil)
+        nib.instantiate(withOwner: self, options: nil)
+        
+        addSubview(contentView)
+//        configureContentView()
+//        configureTextField()
+    }
 }
