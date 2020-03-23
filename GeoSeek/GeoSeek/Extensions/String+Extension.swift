bcf54b261ff8692690df21a8c3c6c1873659f65d
Dan Morse <djmthedev@gmail.com>
2020-03-23 12:08:11 -0700

    removed a comment

--- GeoSeek/GeoSeek/Extensions/String+Extension.swift
+++ GeoSeek/GeoSeek/Extensions/String+Extension.swift
@@ -19,7 +19,6 @@ extension String {
     
     var isValidPassword: Bool {
         //Regex restricts to 8 character minimum, 1 capital letter, 1 lowercase letter, 1 number
-        //If you have different requirements a google search for "password requirement regex" will help
         let passwordFormat      = "(?=.*[A-Z])(?=.*[0-9])(?=.*[a-z]).{8,}"
         let passwordPredicate   = NSPredicate(format: "SELF MATCHES %@", passwordFormat)
         return passwordPredicate.evaluate(with: self)
