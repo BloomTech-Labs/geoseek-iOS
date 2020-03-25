4cac06a9eb156c356f91ba7f28cee2a644981cdd
Dan Morse <djmthedev@gmail.com>
2020-03-25 08:30:16 -0700

    Got Rid of Comments in NetworkController

--- GeoSeek/GeoSeek/Networking/NetworkController.swift
+++ GeoSeek/GeoSeek/Networking/NetworkController.swift
@@ -72,10 +72,6 @@ class NetworkController {
     
     // MARK: - Users
     
-    //        let user = "user123" // also user223, user323, user423, user001, user002, user003, user004, user005, user006
-    //        let password = "aGoodPassword2"
-    //        let email = "email@email.com"
-    
     func register(with username: String, password: String, email: String, completion: @escaping (Result<User, Error>) -> Void) {
         User.removeUser()
         let userToRegister = createUserJSON(username, password, and: email)
@@ -157,7 +153,7 @@ class NetworkController {
     }
     
     // MARK: - Completed Routes
-    // Can't be tested yet.
+    
     func markGemCompleted(_ gem: Gem, comments: String, completion: @escaping (Result<String, Error>) -> Void) {
         guard let user = User.retrieveUser() else {
             completion(.failure(FetchError.noUser))
@@ -245,61 +241,3 @@ class NetworkController {
         }
     }
 }
-
-let thing = """
-COMPLETED
-{
-    "gem_id": 2,
-    "completed_by": 2,
-    "comments": "test2"
-}
-this one is what will be returned:
-{
-  "id": 6,
-  "gem_id": 2,
-  "completed_at": "2020-03-11T22:48:44.197Z",
-  "completed_by": 2,
-  "comments": "test2"
-}
-
-CREATE
- {
-    "title": "testing1",
-    "created_by_user": 6,
-    "longitude": -86.2222,
-    "latitude": 36.3333,
-    "difficulty": 2,
-    "description": "test1"
-}
-
-{
-  "gem": [
-    23
-  ],
-  "message": "Gem was created"
-}
-
-/api/users/register
-{
-    "username": "duds00",
-    "email": "test5@teste21.com",
-    "password": "duds00"
-}
-  {
-    "id": 16,
-    "username": "duds00",
-    "email": "test5@teste21.com"
-  }
-
-/api/users/login
-{
-    "username": "duds00",
-    "password": "duds00"
-}
-{
-  "message": "Welcome duds00!",
-  "token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImR1ZHMwMCIsImlkIjoxNiwiaWF0IjoxNTgzOTQ2Mjk2LCJleHAiOjE1ODQwMzI2OTZ9.0gEq64_fSZtf7qGL7J3ASjGsdPyBZC7WTDKX4IaiQQU",
-  "user_id": 16,
-  "email": "test5@teste21.com"
-}
-"""
